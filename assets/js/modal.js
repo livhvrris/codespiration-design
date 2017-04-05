eb30d5220fe5019ebf705bd48c6f5fc47cac8eb0
Olivia Harris <livhvrris@Olivias-MacBook-Pro.local>
2017-04-05 15:51:26 -0700

    initial design push

---
+++ assets/js/modal.js
@@ -0,0 +1,7 @@
+// ====================
+// Modal Initialization
+// ====================
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
